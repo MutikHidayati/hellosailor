065759d804914d64647874f604215fe492534049
mutikhidayati <mutikhidayati@gmail.com>
2015-08-30 15:55:58 +0700

    Hellosailor versi 1.0.0

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(function() {
+ 
+    //Set up instafeed
+    var feed = new Instafeed({
+        clientId: '7ede2dfafab74de4bc914b7a3458b346',
+        target: 'instafeed',
+        get: 'tagged',
+        tagName: 'sailor',
+        links: true,
+        limit: 8,
+        sortBy: 'most-recent',
+        resolution: 'standard_resolution',
+        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
+    });
+    feed.run();
+ 
+});
